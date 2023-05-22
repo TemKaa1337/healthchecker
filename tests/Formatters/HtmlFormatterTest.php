2d279d717fe054a555c3dba0fead892d047a7e08
TemKaa1337 <temkaaw0wpro@gmail.com>
2023-05-23 00:03:47 +0300

    remove unused whitespaces

--- tests/Formatters/HtmlFormatterTest.php
+++ tests/Formatters/HtmlFormatterTest.php
@@ -1,5 +1,4 @@
-<?php
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 use App\Rules\RuleResult;
 use PHPUnit\Framework\TestCase;
--- tests/HealthCheckerTest.php
+++ tests/HealthCheckerTest.php
@@ -1,7 +1,5 @@
 <?php declare(strict_types=1);
 
-namespace tests;
-
 use App\Contracts\Rule;
 use App\Contracts\RuleResult;
 use App\HealthChecker;
